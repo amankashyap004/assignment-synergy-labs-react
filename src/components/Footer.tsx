06a673e7596e73c4ae6e06ef43b05cffa57dc749
amankashyap004 <amankashyap004@gmail.com>
2024-10-05 22:00:28 +0530

    create: footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
-const Footer = () => {
+const Footer: React.FC = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="container px-4 lg:px-10 py-4 lg:py-6">
+      <div className="flex justify-center items-center">
+        <p className="text-sm lg:text-base opacity-60 font-semibold">
+          &copy; {new Date().getFullYear()} | All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
