a8827a86d82a01558d9ffb97ac33ee2769ab1ccc
amankashyap004 <amankashyap004@gmail.com>
2024-10-05 21:59:57 +0530

    create: apis

---
+++ src/services/userService.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+import { User } from "../types/User";
+
+const API_URL = "https://jsonplaceholder.typicode.com/users";
+
+// Fetch all users
+export const fetchUsers = async (): Promise<User[]> => {
+  const response = await axios.get(API_URL);
+  return response.data;
+};
+
+// Create a new user
+export const createUser = async (user: User): Promise<User> => {
+  const response = await axios.post(API_URL, user);
+  return response.data;
+};
+
+// Update an existing user
+export const updateUser = async (
+  id: number,
+  user: Partial<User>
+): Promise<User> => {
+  const response = await axios.put(`${API_URL}/${id}`, user);
+  return response.data;
+};
+
+// Delete a user
+export const deleteUser = async (userId: number): Promise<void> => {
+  await axios.delete(`${API_URL}/${userId}`);
+};
