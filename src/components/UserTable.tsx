417cd81b0124bcefcdacf45b2aeba8a420352ffc
amankashyap004 <amankashyap004@gmail.com>
2024-10-06 11:15:47 +0530

    add: more features

--- src/components/UserTable.tsx
+++ src/components/UserTable.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { User } from "../types/User";
-import Button from "./ui/Button";
 
 interface UserTableProps {
   users: User[];
